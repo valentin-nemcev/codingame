83766ce1909da39e7ead9380118881256a583259
Валентин Немцев <v-nemcev@yandex-team.ru>
2019-08-16 19:17:44 +0300

    Update config, wip code chages

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,31 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: './tsconfig.json',
-        ecmaVersion: 2018
+        ecmaVersion: 2018,
     },
     env: {
         commonjs: true,
-        node: true
+        node: true,
     },
     extends: [
-        "standard",
-        "plugin:@typescript-eslint/standard",
-        "plugin:@typescript-eslint/recommended"
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier/standard',
     ],
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'prettier'],
     rules: {
-    }
-}
+        'prettier/prettier': [
+            'error',
+            {
+                tabWidth: 4,
+                singleQuote: true,
+                trailingComma: 'all',
+                bracketSpacing: false,
+                parser: 'typescript',
+            },
+        ],
+    },
+};
--- tron.ts
+++ tron.ts
@@ -1,5 +1,4 @@
-const {Console} = require('console');
-declare const readline: () => string
+declare const readline: () => string;
 
 // function last<T>(a: T[]): T | undefined {
 //     return a[a.length - 1]
@@ -17,40 +16,37 @@ function shuffle<T extends Array<unknown>>(a: T): T {
 }
 
 function readInts(): number[] {
-    return readline().split(' ').map(s => parseInt(s, 10));
+    return readline()
+        .split(' ')
+        .map(s => parseInt(s, 10));
 }
-
-
 
 type Dir = 'LEFT' | 'RIGHT' | 'UP' | 'DOWN';
 const dirs: Dir[] = ['LEFT', 'RIGHT', 'UP', 'DOWN'];
 const shift: {[dir in Dir]: (p: Pos) => Pos} = {
-    'LEFT': ({x, y}) => ({x: x - 1, y}),
-    'RIGHT': ({x, y}) => ({x: x + 1, y}),
-    'UP': ({x, y}) => ({x, y: y - 1}),
-    'DOWN': ({x, y}) => ({x, y: y + 1}),
-}
+    LEFT: ({x, y}) => ({x: x - 1, y}),
+    RIGHT: ({x, y}) => ({x: x + 1, y}),
+    UP: ({x, y}) => ({x, y: y - 1}),
+    DOWN: ({x, y}) => ({x, y: y + 1}),
+};
 
 const opposite: {[dir in Dir]: Dir} = {
-    'LEFT': 'RIGHT',
-    'RIGHT': 'LEFT',
-    'UP': 'DOWN',
-    'DOWN': 'UP',
-}
+    LEFT: 'RIGHT',
+    RIGHT: 'LEFT',
+    UP: 'DOWN',
+    DOWN: 'UP',
+};
 
-
-type Way = {start: Pos; dist: number};
-type Poss = {me: Pos; others: Pos[]}
-function readState(): Poss {
+type Input = {myIndex: number; posList: Pos[]};
+function readState(): Input {
     const [playerCount, myIndex] = readInts();
-    const positions = []
+    const posList = [];
     for (let i = 0; i < playerCount; i++) {
-        const [,,x,y] = readInts()
-        positions.push({x,y});
+        const [, , x, y] = readInts();
+        posList.push({x, y});
     }
 
-    const me = positions.splice(myIndex, 1)[0]
-    return {me, others: positions}
+    return {myIndex, posList};
 }
 
 const fieldWidth = 30;
@@ -61,11 +57,10 @@ interface Pos {
     y: number;
 }
 
-type CellState = '' | 'me' | 'other';
-const stateChar: {[state in CellState]: string} = {
-    '': ' ',
-    'me': '0',
-    'other': 'x',
+type CellState = null | number;
+function printState(s: CellState): string {
+    if (s == null) return ' ';
+    else return String(s);
 }
 
 class State {
@@ -75,8 +70,8 @@ class State {
     private grid: CellState[] = [];
 
     constructor() {
-        this.grid.length = fieldWidth * fieldHeight
-        this.grid.fill('')
+        this.grid.length = fieldWidth * fieldHeight;
+        this.grid.fill(null);
     }
 
     private getCell({x, y}: Pos): CellState {
@@ -89,30 +84,34 @@ class State {
 
     dump(): string {
         let result = '';
-        for(let y = 0; y < fieldHeight; y++) {
-            let line = ''
-            for(let x = 0; x < fieldWidth; x++)
-                line += stateChar[this.getCell({x, y})];
-            result += line + "\n";
+        for (let y = 0; y < fieldHeight; y++) {
+            let line = '';
+            for (let x = 0; x < fieldWidth; x++)
+                line += printState(this.getCell({x, y}));
+            result += line + '\n';
         }
         return result;
     }
 
-    append({me, others}: {me: Pos; others: Pos[]}): void {
-        this.me.push(me);
-        others.forEach((pos, i) => {
-            if (!this.others[i]) this.others[i] = []
-            this.others[i].push(pos);
-        })
-        this.setCell(me, 'me');
-        others.forEach(pos => this.setCell(pos, 'other'));
+    append({posList}: Input): void {
+        // const me = posList[myIndex];
+        // this.me.push(me);
+        // others.forEach((pos, i) => {
+        //     if (!this.others[i]) this.others[i] = [];
+        //     this.others[i].push(pos);
+        // });
+        posList.forEach((pos, player) => this.setCell(pos, player));
     }
 
     isPosEmpty({x, y}: Pos): boolean {
-        return 0 <= x && x < fieldWidth && 0 <= y && y < fieldHeight
-            && !this.getCell({x, y})
+        return (
+            0 <= x &&
+            x < fieldWidth &&
+            0 <= y &&
+            y < fieldHeight &&
+            !this.getCell({x, y})
+        );
     }
-
 
     trace(prevPos: Pos, dir: Dir): {dist: number; lastPos: Pos} {
         let dist;
@@ -121,60 +120,67 @@ class State {
             pos = shift[dir](prevPos);
             if (!this.isPosEmpty(pos)) break;
             prevPos = pos;
-            this.setCell(pos, 'me');
+            this.setCell(pos, 1);
         }
         return {dist, lastPos: prevPos};
     }
 
     untrace(pos: Pos, dir: Dir, maxDist: number): void {
-        let dist;
-        for (dist = 0; dist < maxDist; dist++) {
-            this.setCell(pos, '');
+        for (let dist = 0; dist < maxDist; dist++) {
+            this.setCell(pos, null);
             pos = shift[dir](pos);
         }
     }
 }
 
-function bestDir(state: State, pos: Pos, iterationsLeft = 100): {dir: Dir; dist: number} {
+function bestDir(
+    state: State,
+    pos: Pos,
+    iterationsLeft = 16,
+): {dir: Dir; dist: number} {
     let outputDir = dirs[0];
     let maxDist = 0;
-    if (!iterationsLeft) return {dir: outputDir, dist: 0}
+    if (!iterationsLeft) return {dir: outputDir, dist: 0};
     dirs.forEach(dir => {
-        const {dist: firstDist, lastPos} = state.trace(pos, dir)
-        const restDist = firstDist > 0 
-            ? bestDir(state, lastPos, iterationsLeft-1).dist : 0;
-        state.untrace(lastPos, opposite[dir], firstDist)
+        const {dist: firstDist, lastPos} = state.trace(pos, dir);
+        const restDist =
+            firstDist > 0
+                ? bestDir(state, lastPos, iterationsLeft - 1).dist
+                : 0;
+        state.untrace(lastPos, opposite[dir], firstDist);
         const dist = firstDist + restDist;
         if (dist > maxDist) {
             maxDist = dist;
             outputDir = dir;
         }
-    })
+    });
     return {dir: outputDir, dist: maxDist};
 }
 
-const log = console.error
+const log = console.error.bind(console);
 {
     const state = new State();
 
     shuffle(dirs);
 
-    for (let turn = 0;;turn++) {
+    for (let turn = 0; ; turn++) {
+        const input = readState();
+
         const startHr = process.hrtime();
+        const showTime = (): void => {
+            const endHr = process.hrtime(startHr);
+            log('Turn %d: %dms', turn, endHr[1] / 1000000);
+        };
 
-        const endHr = process.hrtime(startHr)
-
-        console.time('Turn ' + turn);
-        const poss = readState();
-        state.append(poss);
-        const {me: myPos} = poss
+        state.append(input);
+        const myPos = input.posList[input.myIndex];
 
         // log(myPos, otherPos);
 
         const {dir} = bestDir(state, myPos);
 
-        log(state.dump())
-        console.log(dir)
-        log('Turn %d: %dms', turn, endHr[1] / 1000)
+        log(state.dump());
+        console.log(dir);
+        showTime();
     }
 }
